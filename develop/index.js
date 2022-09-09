5ee167e64b1c649deb1b02e5eadd09e0c260c9cb
Vincent Toon <vincenttoon22@gmail.com>
2022-09-09 16:09:25 -0500

    main questions added

--- develop/index.js
+++ develop/index.js
@@ -6,6 +6,32 @@ const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please insert your name.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github user name? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please insert your Github user name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
             name: 'project',
             message: 'What is the name of your Project? (Required)',
             validate: projectInput => {
@@ -25,16 +51,101 @@ const questions = () => {
                 if (descriptionInput) {
                     return true;
                 } else {
-                    console.log('Please insert a description');
+                    console.log('Please insert a description.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'gitlink',
+            message: "Please insert a link to this Github Repo. (Required)",
+            validate: gitLinkInput => {
+                if (gitLinkInput) {
+                    return true;
+                } else {
+                    console.log('Please insert this Github Repo');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Please provide step-by-step instructions on how to use your application. (Required)',
+            validate: instInput => {
+                if (instInput) {
+                    return true;
+                } else {
+                    console.log('Please insert valid instructions for this project.');
+                    return false;
+                }
+            }
+        },
+        {
+            name: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please insert a valid usage description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license for this project. (Required)',
+            choices: ['MIT', 'Apache 2.0', 'Creative Commons 1.0', 'GPLv3', 'WTFPL', 'None']
+        },
+        {
+            type: 'confirm',
+            name: 'collaborators',
+            message: 'Did you have other collaborators on this project? (Required)',
+            default: false,
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Please list another collaborator on this project.',
+            when: ({ collaborators }) => {
+                if (collaborators) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            
+        },
+        {
+            type: 'input',
+            name: 'creditsLink',
+            message: 'Please share collaborators Github link',
+            when: ({ credits }) => {
+                if (credits) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'creditConfirm',
+            message: 'Would you like to enter another collaborator?',
+            when: ({ creditsLink }) => {
+                if (creditsLink) {
+                    return true;
+                } else {
                     return false;
                 }
             }
         }
     ])
 };
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 const init = () => {
