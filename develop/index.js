44c193d881781fafd3eaf03a696cf0beac88f265
Vincent Toon <vincenttoon22@gmail.com>
2022-09-09 14:42:17 -0500

    questions registering

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of your Project? (Required)',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description of what your project is, why you did this project, and how it works. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please insert a description');
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions();
+}
 
 // Function call to initialize app
 init();
